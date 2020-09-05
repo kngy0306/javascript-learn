2082483dbbacf2e2870512427cb94ec6a8e1ad92
小長谷祐人 <kona@kona.local>
2020-09-05 20:11:19 +0900

    index,js

---
+++ Pokedex/src/app.ts
@@ -0,0 +1,30 @@
+const container: HTMLElement | any = document.getElementById("app")
+const polemons: number = 100
+
+interface IPokemon{
+    id: number;
+    name: string;
+    image: string;
+    type: string;
+}
+
+const fetchData = (): void => {
+    for(let i = 1; i <= polemons; i++){
+        getPokemon(i)
+    }
+}
+
+const getPokemon = async (id: number): Promise<void> => {
+    const data: Response = await fetch(`https://pokeapi.co/api/v2/pokemon/${id}`)
+    const pokemon: any = await data.json()
+    const pokemonType: string = pokemon.types
+        .map((poke: any) => poke.type.name)
+        .join(",")
+
+    const transformedPokemon = {
+        id: pokemon.id,
+        name: pokemon.name,
+        image: `${pokemon.sprites.front_default}`,
+        type: pokemon.Type,
+    }
+}
