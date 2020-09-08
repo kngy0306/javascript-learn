ad0426c400427aa9896a27a1cfb801f8fdd73b1a
Kona <kitami.kitano20@gmail.com>
2020-09-08 11:38:09 +0900

    useing PokeAPI APP

--- initTS/greeter.ts
+++ initTS/greeter.ts
@@ -1,17 +1,21 @@
 class Student {
-    fullName: string;
-    constructor(public firstName: string, public middleInitial: string, public lastName: string){
-        this.fullName = firstName + " " + middleInitial + " " + lastName;
-    }
+  fullName: string;
+  constructor(
+    public firstName: string,
+    public middleInitial: string,
+    public lastName: string
+  ) {
+    this.fullName = firstName + " " + middleInitial + " " + lastName;
+  }
 }
 
-interface Person{
-    firstName: string;
-    lastName: string;
+interface Person {
+  firstName: string;
+  lastName: string;
 }
 
-function greeter(person: Person){
-    return "Helo, " + person.firstName + " " + person.lastName;
+function greeter(person: Person) {
+  return "Hello, " + person.firstName + " " + person.lastName;
 }
 
 let user = new Student("Kona", "nakada", "yuto");
