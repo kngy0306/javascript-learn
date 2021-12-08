52e4b20f0febcf94573fc702e2042f3f4c8ee572
Kona <kitami.kitano20@gmail.com>
2021-12-08 23:10:15 +0900

    :tada: twitterapi

---
+++ twitter_api/README.md
@@ -0,0 +1,11 @@
+## TwitterAPI v2
+
+ドキュメント: tweets/search/recent
+[GET /2/tweets/search/recent](https://developer.twitter.com/en/docs/twitter-api/tweets/search/api-reference/get-tweets-search-recent)
+
+ドキュメント: クエリ
+https://developer.twitter.com/en/docs/twitter-api/tweets/search/integrate/build-a-query
+
+nodeで環境変数を利用する
+[Node.jsで環境変数を利用する方法](https://www.twilio.com/blog/working-with-environment-variables-in-node-js-html-jp)
+
---
+++ twitter_api/index.js
@@ -0,0 +1,47 @@
+const needle = require('needle');
+
+if (process.env.NODE_ENV !== 'production') {
+  require('dotenv').config();
+}
+
+const token = process.env.BEARER_TOKEN;
+const endpointUrl = "https://api.twitter.com/2/tweets/search/recent";
+
+async function getRequest() {
+
+    // Edit query parameters below
+    // specify a search query, and any additional fields that are required
+    // by default, only the Tweet ID and text fields are returned
+    const params = {
+        'query': 'from:twitterdev -is:retweet',
+        'tweet.fields': 'author_id'
+    }
+
+    const res = await needle('get', endpointUrl, params, {
+        headers: {
+            "User-Agent": "v2RecentSearchJS",
+            "authorization": `Bearer ${token}`
+        }
+    })
+
+    if (res.body) {
+        return res.body;
+    } else {
+        throw new Error('Unsuccessful request');
+    }
+}
+
+(async () => {
+
+    try {
+        const response = await getRequest();
+        console.dir(response, {
+            depth: null
+        });
+
+    } catch (e) {
+        console.log(e);
+        process.exit(-1);
+    }
+    process.exit();
+})();
